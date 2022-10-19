af71ab879718841b019f7dc3ee800c16f25f6a86
Devin Patterson <dzpatterson15@gmail.com>
2022-10-18 21:07:23 -0600

    udate to JS

--- js/cards.js
+++ js/cards.js
@@ -1,24 +1,42 @@
 // Variable for the User's Zip Code//
 
-
-const user_zip = 
-
+user_zip = "84128"
 
 // API URL, API-KEY, & Fetch//
 const options = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': '#',
+		'X-RapidAPI-Key': 'fe226f124cmshd8ec7c39c68353ap1855c0jsna94353498904',
 		'X-RapidAPI-Host': 'restaurants-near-me-usa.p.rapidapi.com'
-	}
-};
+	}};
 
 fetch(`https://restaurants-near-me-usa.p.rapidapi.com/restaurants/location/zipcode/${user_zip}/0`, options)
 	.then(response => response.json())
 	.then(response => console.log(response))
-	.catch(err => console.error(err));
+	.then(function (jsonObject) {
+		const restaurants = ['restaurants'];
+		const cards = document.querySelector('.cards');
 
-// CONVERT TO JSON//
+		restaurants.array.forEach(element => {
+			let card = document.createElement('section');
+			let h2 = document.createElement('h2');
+			let p1 = document.createElement('p');
+			let p2 =  document.createElement('p');
+			let p3 = document.createElement('p');
+			let p4 = document.createElement('p');
 
+			h2.textContent = `${restaurants.restaurantName}`;
+			p1.textContent = `${restaurants.hoursInterval}`;
+			p2.textContent = `${restaurants.address}`;
+			p3.textContent = `${restaurants.phone}`;
+			p4.textContent = `${restaurants.website}`;
 
-//Do a for/then loop to create html cards//
+			card.append(h2);
+			card.append(p1);
+			card.append(p2);
+			card.append(p3);
+			card.append(p4);
+			cards.append(card);
+		});
+
+});
