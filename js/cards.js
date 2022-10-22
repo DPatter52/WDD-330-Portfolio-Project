a7b58b55803cd5a8cc5ab5939d4ca0ad8ba6c499
Devin Patterson <dzpatterson15@gmail.com>
2022-10-22 04:58:32 -0600

    Huge update to javascript and css

--- js/cards.js
+++ js/cards.js
@@ -1,44 +1,46 @@
-
-let key = "3a94406d73mshb1d2ba951df67fbp13b018jsn2a0cc1fd637f"
-const cards = document.querySelector('.cards');
+let key = "3a94406d73mshb1d2ba951df67fbp13b018jsn2a0cc1fd637f";
+const cards = document.getElementById("cards");
 
 function getRestaurants() {
-	let userinput = document.getElementById('userinput').value;
+  let userinput = document.getElementById("userinput").value;
 
-	fetch(`https://restaurants-near-me-usa.p.rapidapi.com/restaurants/location/zipcode/${userinput}/0`, {
-		method: 'GET',
-		headers: {
-			'X-RapidAPI-Key': key,
-			'X-RapidAPI-Host': 'restaurants-near-me-usa.p.rapidapi.com'
-		}
-	})
-		.then(response => response.json())
-		.then(response => console.log(response))
-		.then(response => showRestaurants(response))
-		.catch(err => console.error(err));
-};
+  fetch(
+    `https://restaurants-near-me-usa.p.rapidapi.com/restaurants/location/zipcode/${userinput}/0`,
+    {
+      method: "GET",
+      headers: {
+        "X-RapidAPI-Key": key,
+        "X-RapidAPI-Host": "restaurants-near-me-usa.p.rapidapi.com",
+      },
+    }
+  )
+    .then((response) => response.json())
+    .then((response) => showRestaurants(response))
+    // .then(response => console.log(response))
+    .catch((err) => console.error(err));
+}
 
 function showRestaurants(response) {
-	response.restaurants.forEach(restaurant => {
-		let card = document.createElement('section');
-		let h2 = document.createElement('h2');
-		let p1 = document.createElement('p1');
-		let p2 = document.createElement('p2');
-		let p3 = document.createElement('p3');
-		let p4 = document.createElement('p4');
+  response.restaurants.forEach((restaurant) => {
+    let card = document.createElement("section");
+    let h2 = document.createElement("h2");
+    let p1 = document.createElement("p1");
+    let p2 = document.createElement("p2");
+    let p3 = document.createElement("p3");
+    let p4 = document.createElement("p4");
 
-		h2.innerHTML = `${restaurant.restaurantName}`;
-		p1.innerHTML = `${restaurant.hoursInterval}`;
-		p2.innerHTML = `${restaurant.phone}`;
-		p3.innerHTML = `${restaurant.address}`;
-		p4.innerHTML = `${restaurant.website}`;
+    h2.innerHTML = `${restaurant.restaurantName}`;
+    p1.innerHTML = `<strong>Business Hours: </strong>${restaurant.hoursInterval}`;
+    p2.innerHTML = `<strong>Phone Number: </strong>${restaurant.phone}`;
+    p3.innerHTML = `<strong>Address: </strong>${restaurant.address}`;
+    p4.innerHTML = `<strong>Website: </strong>${restaurant.website}`;
 
-		card.append(h2);
-		card.append(p1);
-		card.append(p2);
-		card.append(p3);
-		card.append(p4);
+    card.append(h2);
+    card.append(p1);
+    card.append(p2);
+    card.append(p3);
+    card.append(p4);
 
-		cards.appendChild(card);	
-	});
-};
+    cards.appendChild(card);
+  });
+}
--- js/date.js
+++ js/date.js
@@ -1,9 +1,12 @@
-var date = (new Date().getFullYear());
+var date = new Date().getFullYear();
 document.getElementById("year").innerHTML = date;
 
-
-var currentdate = new Date().toLocaleDateString('en-GB', {weekday:"long", year:"numeric", month:"long", day:"numeric"});
+var currentdate = new Date().toLocaleDateString("en-GB", {
+  weekday: "long",
+  year: "numeric",
+  month: "long",
+  day: "numeric",
+});
 document.getElementById("currentdate").innerHTML = currentdate;
-
 
 document.write("Last Modified: " + document.lastModified);
