455b1d7750210e7980dd0450e1b83cbefbbf2cf5
Devin Patterson <dzpatterson15@gmail.com>
2022-10-26 02:41:31 -0600

    update to javascript for card functionality

--- js/cards.js
+++ js/cards.js
@@ -2,7 +2,6 @@ let key = "be9e7f74d1msh79cbc6a843f61cep1e5f3ajsn149566766ca4";
 const cards = document.getElementById("cards");
 
 function getRestaurants() {
-
   let userinput = document.getElementById("userinput").value;
 
   fetch(
@@ -22,17 +21,30 @@ function getRestaurants() {
 }
 
 function showRestaurants(response) {
+  cards.innerHTML = "";
 
   response.restaurants.forEach((restaurant) => {
     let card = document.createElement("section");
+    card.setAttribute("id", "card");
     let h2 = document.createElement("h2");
+    h2.setAttribute("id", "restaurantName");
     let p1 = document.createElement("p1");
+    p1.setAttribute("id", "hours");
     let p2 = document.createElement("p2");
+    p2.setAttribute("id", "phone");
     let p3 = document.createElement("p3");
+    p3.setAttribute("id", "address");
     let p4 = document.createElement("p4");
+    p4.setAttribute("id", "website");
 
     h2.innerHTML = `${restaurant.restaurantName}`;
-    p1.innerHTML = `<strong>Business Hours: </strong>${restaurant.hoursInterval}`;
+
+    if (restaurant.hoursInterval == "") {
+      p1.innerHTML = `<strong>Business Hours: </strong>N/A`;
+    } else {
+      p1.innerHTML = `<strong>Business Hours: </strong>${restaurant.hoursInterval}`;
+    }
+
     p2.innerHTML = `<strong>Phone Number: </strong>${restaurant.phone}`;
     p3.innerHTML = `<strong>Address: </strong>${restaurant.address}`;
     p4.innerHTML = `<strong>Website: </strong>${restaurant.website}`;
